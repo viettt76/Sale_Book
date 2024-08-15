d36746eb0034b249e1c68ea7f682b4b957516d36
TranVanAnh <anh038953@gmail.com>
2024-08-15 22:12:46 +0700

    fix error search by book group

--- Backend/BookStore.WebApi/BookStore.Bussiness/Services/BookService.cs
+++ Backend/BookStore.WebApi/BookStore.Bussiness/Services/BookService.cs
@@ -141,7 +141,7 @@ namespace BookStore.Bussiness.Services
                 {
                     entities = entities.Where(x => 
                         x.Title.ToLower().Contains(spec.Filter.ToLower()) ||
-                        x.BookAuthors.Any(x => x.Author.FullName.Contains(spec.Filter))
+                        x.BookAuthors.Any(x => x.Author.FullName.ToLower().Contains(spec.Filter.ToLower()))
                     );
                 }
 
@@ -157,7 +157,7 @@ namespace BookStore.Bussiness.Services
 
                 if (spec.BookGroupIds != null)
                 {
-                    entities = entities.Where(x => x.BookAuthors.Any(ba => spec.BookGroupIds.Contains(ba.AuthorId)));
+                    entities = entities.Where(x => spec.BookGroupIds.Contains(x.BookGroupId));
                 }
 
                 entities = spec.Sorting switch
