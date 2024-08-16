22102f50704b915d951209ae22cdf68dd945f741
TranVanAnh <anh038953@gmail.com>
2024-08-16 09:19:07 +0700

    fix lỗi đặt hàng ở cart nhwung k mất sách ở cart

--- Backend/BookStore.WebApi/BookStore.Datas/Interfaces/ICartItemRepository.cs
+++ Backend/BookStore.WebApi/BookStore.Datas/Interfaces/ICartItemRepository.cs
@@ -4,6 +4,7 @@ namespace BookStore.Datas.Interfaces
 {
     public interface ICartItemRepository : IBaseRepository<CartItem>
     {
+        Task<CartItem> GetCartItemAsync(int cartId, int bookId);
         Task<CartItem> AddToCart(CartItem cartItem);
         Task<CartItem> GetCartItemIsExist(int bookId, int cartId, string userId);
         Task<CartItem> UpdateCartItemQuantity(int bookId, int cartid, int quantity, string userid);
