146780a0299eb87ca1f75f1d8d3a6d15d317ca3c
viettt76 <viettt76@gmail.com>
2024-08-16 17:29:08 +0700

    axios

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -8,44 +8,47 @@ const instance = axios.create({
     baseURL: 'https://dfr33pu4ekpqr2hn5z5wm2k7cq0mzfri.lambda-url.us-east-1.on.aws/api/v1',
 });
 
-instance.defaults.withCredentials = true;
+export const SetupInterceptors = (navigate) => {
+    instance.defaults.withCredentials = true;
 
-instance.interceptors.request.use(
-    function (config) {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+    instance.interceptors.request.use(
+        function (config) {
+            const token = localStorage.getItem('token');
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
 
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
-    },
-);
+            return config;
+        },
+        function (error) {
+            return Promise.reject(error);
+        },
+    );
 
-instance.interceptors.response.use(
-    function (response) {
-        const customResponse = {
-            data: response.data,
-            status: response.status,
-        };
-        return customResponse;
-    },
-    function (error) {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            window.location.href = '/login';
-            store.dispatch(clearUserInfo());
-            customToastify.info('Bạn đã hết phiên đăng nhập');
-        }
-        if (error?.response?.data)
-            return Promise.reject({
-                status: error?.response?.status,
-                data: error?.response?.data,
-            });
-        return Promise.reject(error);
-    },
-);
+    instance.interceptors.response.use(
+        function (response) {
+            const customResponse = {
+                data: response.data,
+                status: response.status,
+            };
+            return customResponse;
+        },
+        function (error) {
+            if (error.response?.status === 401) {
+                localStorage.removeItem('token');
+                // window.location.href = '/login';
+                navigate('/login');
+                store.dispatch(clearUserInfo());
+                customToastify.info('Bạn đã hết phiên đăng nhập');
+            }
+            if (error?.response?.data)
+                return Promise.reject({
+                    status: error?.response?.status,
+                    data: error?.response?.data,
+                });
+            return Promise.reject(error);
+        },
+    );
+};
 
 export default instance;
